2050404e934fc5e88eb91d9655d5bdbe85cc811b
KonAsa <you@example.com>
2023-02-16 06:59:51 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * return: 0
+ * main - Entry point
  *
-int main (void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-        puts(uts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
