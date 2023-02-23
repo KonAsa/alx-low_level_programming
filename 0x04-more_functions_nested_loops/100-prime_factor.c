a43b9f09877a6934d5058013558b4c815188a522
KonAsa <you@example.com>
2023-02-23 06:55:11 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+
 /**
- * main - prints prime factors of 612852475143
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Description: Finds the highest prime factor of a number
+ * Return: Always 0.
  */
+
 int main(void)
 {
+	long i, number = 612852475143;
+
+	for (i  = 2; i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			number = number / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
 	return (0);
 }
