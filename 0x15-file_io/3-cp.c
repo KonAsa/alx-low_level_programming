32e2fee9616d0f2fccdc3171f45525e5dbecac38
KonAsa <konas32@gmail.com>
2023-04-08 18:24:29 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,83 +1,65 @@
 #include "main.h"
 
-#define BUF_SIZE 1024
+#define BUFFER_SIZE 1024
 
-/**
- * print_error - prints an error message and exits with the given code
- * @code: exit code
- * @msg: error message
- * @arg: argument to include in the error message
- */
-void print_error(int code, const char *msg, const char *arg)
-{
-	dprintf(STDERR_FILENO, msg, arg);
-	exit(code);
+void copy_file(const char *source_file, const char *destination_file);
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        dprintf(STDERR_FILENO, "Usage: cp SOURCE DEST\n");
+        exit(EXIT_FAILURE);
+    }
+
+    const char *source_file = argv[1];
+    const char *destination_file = argv[2];
+
+    copy_file(source_file, destination_file);
+
+    return 0;
 }
 
-/**
- * open_file - opens a file with the given mode
- * @filename: file to open
- * @mode: file mode
- * @code: exit code to use in case of error
- *
- * Return: file descriptor
- */
-int open_file(const char *filename, int mode, int code)
-{
-	int fd = open(filename, mode);
+void copy_file(const char *source_file, const char *destination_file) {
+    int source_fd = open(source_file, O_RDONLY);
+    if (source_fd == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't open %s for reading\n", source_file);
+        exit(EXIT_FAILURE);
+    }
 
-	if (fd == -1)
-		print_error(code, "Error: Can't open file %s\n", filename);
+    int destination_fd = open(destination_file, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (destination_fd == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't open %s for writing\n", destination_file);
+        close(source_fd);
+        exit(EXIT_FAILURE);
+    }
 
-	return (fd);
-}
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read, bytes_written;
 
-/**
- * copy_file - copies the contents of one file to another
- * @src: source file
- * @dest: destination file
- */
-void copy_file(const char *src, const char *dest)
-{
-	char buf[BUF_SIZE];
-	ssize_t bytes_read, bytes_written;
-	int fd_src, fd_dest;
+    while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0) {
+        bytes_written = write(destination_fd, buffer, bytes_read);
+        if (bytes_written != bytes_read) {
+            dprintf(STDERR_FILENO, "Error: Write failed\n");
+            close(source_fd);
+            close(destination_fd);
+            exit(EXIT_FAILURE);
+        }
+    }
 
-	fd_src = open_file(src, O_RDONLY, 98);
-	fd_dest = open_file(dest, O_WRONLY | O_CREAT | O_TRUNC, 99);
+    if (bytes_read == -1) {
+        dprintf(STDERR_FILENO, "Error: Read failed\n");
+        close(source_fd);
+        close(destination_fd);
+        exit(EXIT_FAILURE);
+    }
 
-	while ((bytes_read = read(fd_src, buf, BUF_SIZE)) > 0)
-	{
-		bytes_written = write(fd_dest, buf, bytes_read);
+    if (close(source_fd) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close %s\n", source_file);
+        exit(EXIT_FAILURE);
+    }
 
-		if (bytes_written == -1)
-			print_error(99, "Error: Can't write to %s\n", dest);
-	}
-
-	if (bytes_read == -1)
-		print_error(98, "Error: Can't read from file %s\n", src);
-
-	if (close(fd_src) == -1)
-		print_error(100, "Error: Can't close fd %d\n", fd_src);
-
-	if (close(fd_dest) == -1)
-		print_error(100, "Error: Can't close fd %d\n", fd_dest);
-}
-
-/**
- * main - entry point
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success, 97-100 on error
- */
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-		print_error(97, "Usage: cp file_from file_to\n", NULL);
-
-	copy_file(argv[1], argv[2]);
-
-	return (0);
+    if (close(destination_fd) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close %s\n", destination_file);
+        exit(EXIT_FAILURE);
+    }
 }
 
